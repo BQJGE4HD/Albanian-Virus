e1bcf1fc16d3fc4495cdf7ce8b82f707b292e622
BQJGE4HD <juricarpinteiro@gmail.com>
2024-05-18 19:40:49 +0200

    Add files via upload

---
+++ albanian virus.bat
@@ -0,0 +1,12 @@
+@echo off
+title PR0JECT_SAAS
+color 0a
+
+cd %localhost%
+echo do >>albanian_virus.vbs
+cd %localhost%
+echo x=MsgBox("Hi, I am an Albanian virus but because of poor technology in my country unfortunatley I am not able to harm your computer. Please be so kind to delete one of your important files yourself and then forward me to other users. Many thanks for your cooperation! Best regards Albanian virus ", 0+48, "Albanian virus")>>albanian_virus.vbs
+cd %localhost%
+echo loop >>albanian_virus.vbs
+cd %localhost%
+start albanian_virus.vbs
